9afdc7e82629116fad70445b4479da94355d6b05
Ghandi-dev <scoutghandi31@gmail.com>
2025-05-18 10:26:25 +0700

    fix: update payment status checks in dashboard summary calculations

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,30 +5,36 @@ import * as schema from "../models/index";
 import { CA_CERTIFICATE, DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_USER } from "../utils/env";
 const CA_CERTIFICATION = CA_CERTIFICATE;
 
+const isLocal = DATABASE_HOST === "localhost";
+
 const client = new Pool({
-  host: DATABASE_HOST,
-  port: DATABASE_PORT,
-  user: DATABASE_USER,
-  password: DATABASE_PASSWORD,
-  database: DATABASE_NAME,
-  ssl: {
-    ca: CA_CERTIFICATION,
-    rejectUnauthorized: false,
-  },
+	host: DATABASE_HOST,
+	port: DATABASE_PORT,
+	user: DATABASE_USER,
+	password: DATABASE_PASSWORD,
+	database: DATABASE_NAME,
+	...(isLocal
+		? {} // tanpa SSL untuk localhost
+		: {
+				ssl: {
+					ca: CA_CERTIFICATION,
+					rejectUnauthorized: false,
+				},
+		  }),
 });
 
 export const db = drizzle(client, { schema });
 
 const checkConnection = async () => {
-  try {
-    await client.query("SELECT 1"); // ✅ query test
-    console.log("✅ Database connected successfully!");
-  } catch (error) {
-    console.log(DATABASE_HOST, DATABASE_PORT, DATABASE_USER, DATABASE_NAME, DATABASE_PASSWORD);
+	try {
+		await client.query("SELECT 1"); // ✅ query test
+		console.log("✅ Database connected successfully!");
+	} catch (error) {
+		console.log(DATABASE_HOST, DATABASE_PORT, DATABASE_USER, DATABASE_NAME, DATABASE_PASSWORD);
 
-    console.error("❌ Database connection failed:", error);
-    process.exit(1);
-  }
+		console.error("❌ Database connection failed:", error);
+		process.exit(1);
+	}
 };
 
 export { checkConnection };
--- src/service/dashboard.service.ts
+++ src/service/dashboard.service.ts
@@ -1,63 +1,63 @@
 import { and, count, eq, sql, sum } from "drizzle-orm";
 import { db } from "../db";
-import { payment, santri, SANTRI_STATUS, TYPE_PAYMENT } from "../models";
+import { payment, santri, SANTRI_STATUS, STATUS_PAYMENT, TYPE_PAYMENT } from "../models";
 
 const dashboardService = {
-  async getDashboardSummaryAdmin() {
-    const currentYear = new Date().getFullYear();
-    const minYear = currentYear - 9;
+	async getDashboardSummaryAdmin() {
+		const currentYear = new Date().getFullYear();
+		const minYear = currentYear - 9;
 
-    const [{ count: totalSantri }] = await db.select({ count: count() }).from(santri).where(eq(santri.status, SANTRI_STATUS.ACTIVE));
+		const [{ count: totalSantri }] = await db.select({ count: count() }).from(santri).where(eq(santri.status, SANTRI_STATUS.ACTIVE));
 
-    const [{ sum: totalRegistrasi }] = await db
-      .select({ sum: sum(payment.amount).as("sum") })
-      .from(payment)
-      .where(eq(payment.type, TYPE_PAYMENT.REGISTRATION));
+		const [{ sum: totalRegistrasi }] = await db
+			.select({ sum: sum(payment.amount).as("sum") })
+			.from(payment)
+			.where(and(eq(payment.type, TYPE_PAYMENT.REGISTRATION), eq(payment.status, STATUS_PAYMENT.COMPLETED)));
 
-    const [{ sum: totalSPP }] = await db
-      .select({ sum: sum(payment.amount).as("sum") })
-      .from(payment)
-      .where(eq(payment.type, TYPE_PAYMENT.SPP));
+		const [{ sum: totalSPP }] = await db
+			.select({ sum: sum(payment.amount).as("sum") })
+			.from(payment)
+			.where(and(eq(payment.type, TYPE_PAYMENT.SPP), eq(payment.status, STATUS_PAYMENT.COMPLETED)));
 
-    const santriPerYear = await db
-      .select({
-        year: sql<string>`EXTRACT(YEAR FROM ${santri.createdAt})::TEXT`.as("year"),
-        count: count().as("count"),
-      })
-      .from(santri)
-      .where(and(eq(santri.status, SANTRI_STATUS.ACTIVE), sql`EXTRACT(YEAR FROM ${santri.createdAt}) >= ${minYear}`))
-      .groupBy(sql`EXTRACT(YEAR FROM ${santri.createdAt})`)
-      .orderBy(sql`year`);
+		const santriPerYear = await db
+			.select({
+				year: sql<string>`EXTRACT(YEAR FROM ${santri.createdAt})::TEXT`.as("year"),
+				count: count().as("count"),
+			})
+			.from(santri)
+			.where(and(eq(santri.status, SANTRI_STATUS.ACTIVE), sql`EXTRACT(YEAR FROM ${santri.createdAt}) >= ${minYear}`))
+			.groupBy(sql`EXTRACT(YEAR FROM ${santri.createdAt})`)
+			.orderBy(sql`year`);
 
-    const registrasiPerYear = await db
-      .select({
-        year: sql<string>`EXTRACT(YEAR FROM ${payment.createdAt})::TEXT`.as("year"),
-        sum: sum(payment.amount).as("sum"),
-      })
-      .from(payment)
-      .where(and(eq(payment.type, TYPE_PAYMENT.REGISTRATION), sql`EXTRACT(YEAR FROM ${payment.createdAt}) >= ${minYear}`))
-      .groupBy(sql`EXTRACT(YEAR FROM ${payment.createdAt})`)
-      .orderBy(sql`year`);
+		const registrasiPerYear = await db
+			.select({
+				year: sql<string>`EXTRACT(YEAR FROM ${payment.createdAt})::TEXT`.as("year"),
+				sum: sum(payment.amount).as("sum"),
+			})
+			.from(payment)
+			.where(and(eq(payment.type, TYPE_PAYMENT.REGISTRATION), sql`EXTRACT(YEAR FROM ${payment.createdAt}) >= ${minYear}`))
+			.groupBy(sql`EXTRACT(YEAR FROM ${payment.createdAt})`)
+			.orderBy(sql`year`);
 
-    const sppPerYear = await db
-      .select({
-        year: sql<string>`EXTRACT(YEAR FROM ${payment.createdAt})::TEXT`.as("year"),
-        sum: sum(payment.amount).as("sum"),
-      })
-      .from(payment)
-      .where(and(eq(payment.type, TYPE_PAYMENT.SPP), sql`EXTRACT(YEAR FROM ${payment.createdAt}) >= ${minYear}`))
-      .groupBy(sql`EXTRACT(YEAR FROM ${payment.createdAt})`)
-      .orderBy(sql`year`);
+		const sppPerYear = await db
+			.select({
+				year: sql<string>`EXTRACT(YEAR FROM ${payment.createdAt})::TEXT`.as("year"),
+				sum: sum(payment.amount).as("sum"),
+			})
+			.from(payment)
+			.where(and(eq(payment.type, TYPE_PAYMENT.SPP), sql`EXTRACT(YEAR FROM ${payment.createdAt}) >= ${minYear}`))
+			.groupBy(sql`EXTRACT(YEAR FROM ${payment.createdAt})`)
+			.orderBy(sql`year`);
 
-    return {
-      totalSantri,
-      totalRegistrasi: totalRegistrasi ?? 0,
-      totalSPP: totalSPP ?? 0,
-      santriPerYear,
-      registrasiPerYear,
-      sppPerYear,
-    };
-  },
+		return {
+			totalSantri,
+			totalRegistrasi: totalRegistrasi ?? 0,
+			totalSPP: totalSPP ?? 0,
+			santriPerYear,
+			registrasiPerYear,
+			sppPerYear,
+		};
+	},
 };
 
 export default dashboardService;
